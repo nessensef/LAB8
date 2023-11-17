142dea8f591b96d8f04065ef09310f99dfe0baa5
12 <12@DESKTOP-K6U2P3E>
2023-11-17 10:02:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("x и y");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+try
+{
+    int z = x / y;
+}
+catch (DivideByZeroException) when (y == 0)
+{
+    Console.WriteLine("Ошибка");
+}
+try
+{
+    int z = x / y;
+    if (x == 0)
+    {
+        throw new Exception("x ");
+    }
+    else
+    {
+        Console.WriteLine($":{z}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
